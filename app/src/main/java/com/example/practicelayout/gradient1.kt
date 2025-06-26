eb7809b2ca45e7f77e9f42c07acfb6af7136867b
Diya3766 <diyaxx028766@gmail.com>
2025-06-26 08:52:52 +0530

    Update gradient1.kt

--- app/src/main/java/com/example/practicelayout/gradient1.kt
+++ app/src/main/java/com/example/practicelayout/gradient1.kt
@@ -1,12 +1,15 @@
 package com.example.practicelayout
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class gradient1 : AppCompatActivity() {
+    var btn:Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +18,9 @@ class gradient1 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        }
         }
     }
 }
