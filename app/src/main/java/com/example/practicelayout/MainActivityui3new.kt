83059da772e5c2360ee683b030fa85837a1f3a40
Diya3766 <diyaxx028766@gmail.com>
2025-06-26 08:53:28 +0530

    Create MainActivityui3new.kt

---
+++ app/src/main/java/com/example/practicelayout/MainActivityui3new.kt
@@ -0,0 +1,28 @@
+package com.example.practicelayout
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivityui3new : AppCompatActivity() {
+    var btn:Button?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_activityui3new)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        btn=findViewById<>(R.id.btnLogin)
+        btn?.setOnClickListener {
+            Toast.makeText(this,"Loading",Toast.LENGTH_SHORT).show()
+            var intent+
+        }
+    }
+}
