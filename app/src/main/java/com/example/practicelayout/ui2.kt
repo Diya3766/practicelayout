0ae15ebbacb468efa324bd763894328056ed607c
Diya3766 <diyaxx028766@gmail.com>
2025-06-26 08:53:36 +0530

    Create ui2.kt

---
+++ app/src/main/java/com/example/practicelayout/ui2.kt
@@ -0,0 +1,32 @@
+package com.example.practicelayout
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ui2 : AppCompatActivity() {
+    var btn: Button?= null
+    var bttn: Button?= null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ui2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        btn=findViewById(R.id.btnLogin)
+        btn?.setOnClickListener {
+            Toast.makeText(this,"Loading",Toast.LENGTH_SHORT).show()
+            var intent=
+
+        }
+
+    }
+}
